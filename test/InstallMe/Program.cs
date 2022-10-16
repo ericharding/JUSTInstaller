a982a9a034854981398b08c95bb7138b5a6e9a76
Eric Harding <eharding@blackedge.com>
2022-10-16 17:55:59 -0400

    Test IsfirstRunOnCurrentVersion

--- test/InstallMe/Program.cs
+++ test/InstallMe/Program.cs
@@ -3,16 +3,15 @@ using static System.Environment;
 
 await main();
 
-static async Task main()
-{
+static async Task main() {
     Installer installer = new Installer(new InstallerConfig(
-        EntryPoint:"InstallMe", 
-        InstallBasePath:"~/apps/install_me", 
-        InstallFolderTemplate:"version_{version}",
-        CurrentVersionUri:new Uri("http://digitalsorcery.net/InstallMe/version.txt"), 
-        UpdateLocationTemplate:"http://digitalsorcery.net/InstallMe/download/InstallMe_{version}.zip",
+        EntryPoint: "InstallMe",
+        InstallBasePath: "~/apps/install_me",
+        InstallFolderTemplate: "version_{version}",
+        CurrentVersionUri: new Uri("http://digitalsorcery.net/InstallMe/version.txt"),
+        UpdateLocationTemplate: "http://digitalsorcery.net/InstallMe/download/InstallMe_{version}.zip",
         // WindowsShortcutPaths are automatically ignored if not installing to Windows
-        WindowsShortcutPaths: new [] {
+        WindowsShortcutPaths: new[] {
             Path.Combine(Environment.GetFolderPath(SpecialFolder.Desktop), "InstallMe")
         },
         // SymlinkPaths are created on all platforms
@@ -22,20 +21,25 @@ static async Task main()
     installer.OnError += Console.WriteLine;
     installer.OnInfo += Console.WriteLine;
 
+    if (installer.IsfirstRunOnCurrentVersion) {
+        Console.WriteLine("FIRST RUN THIS VERSION!");
+    }
+
     Console.WriteLine($"Current version {installer.CurrentVersion}");
     Console.Write($"Update available? ");
     bool updateAvailable = await installer.CheckforUpdate();
     if (updateAvailable) {
         Console.WriteLine($"Yes - {installer.AvailableVersion}");
-    } else { Console.WriteLine("No"); }
+    }
+    else { Console.WriteLine("No"); }
 
     var resp = Prompt("Update now?", "y", "n");
     if (resp == "y") {
-        await installer.InstallUpdate(run:false);
+        await installer.InstallUpdate(run: false);
     }
 
     bool running = true;
-    while(running) {
+    while (running) {
         running = Prompt("Quit?", "y", "n") == "n";
     }
 }
@@ -43,7 +47,7 @@ static async Task main()
 static string Prompt(string prompt, params string[] options) {
     Console.WriteLine($"{prompt} {string.Join('/', options)}");
     string? ans = null;
-    while(!options.Contains(ans)) {
+    while (!options.Contains(ans)) {
         try {
             ans = Console.ReadLine();
         } catch (IOException) {
